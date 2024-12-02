1313f05b615c8a8e459c154f2a7f791918087e55
Ruturaj <kruturaj96@gmail.com>
2024-12-02 20:58:11 +0530

    Updated logo

--- src/components/Navabr.tsx
+++ src/components/Navabr.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-800 shadow-md py-4 px-6">
         <div className="container mx-auto flex justify-between items-center">
-          <div className="text-2xl font-bold text-teal-400">Mindfulspace</div>
+          <div className="text-2xl font-bold text-teal-400">Mindtechwellness</div>
           <div className="flex space-x-8">
             <Link href="/" className="flex items-center text-gray-300 hover:text-teal-400 transition duration-300">
               <Home className="mr-2" size={20} />
