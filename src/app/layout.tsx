ce74d5121944be9dace610dab1581144a4d6db81
Ruturaj <kruturaj96@gmail.com>
2024-12-04 13:17:29 +0530

    Updated head tag1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,26 @@
-
-import "./globals.css";
-import Script from "next/script";
-
+import './globals.css'
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <head>
         {/* Google Analytics Script */}
         <title>Mindtechwellness</title>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-CXXLD2GZQE"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CXXLD2GZQE"></script>
+        <script>
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-CXXLD2GZQE');
-          `}
-        </Script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CXXLD2GZQE');
+  `}
+        </script>
       </head>
       <body>{children}</body>
     </html>
-  );
+  )
 }
