99f0a7e2d72d946a920e257cdd4e465d94443c75
Ruturaj <kruturaj96@gmail.com>
2024-12-04 11:56:10 +0530

    Added Google Analytics

--- src/components/Navabr.tsx
+++ src/components/Navabr.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className="bg-gray-800 shadow-md py-4 px-6">
         <div className="container mx-auto flex justify-between items-center">
           <div className="text-2xl font-bold text-teal-400">Mindtechwellness</div>
-          <div className="flex space-x-8">
+          <div className="flex space-x-2">
             <Link href="/" className="flex items-center text-gray-300 hover:text-teal-400 transition duration-300">
               <Home className="mr-2" size={20} />
               Home
